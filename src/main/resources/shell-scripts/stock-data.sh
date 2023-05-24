c214e31de8f06afe3cbf811ed40518f182866136
anshuman.saxena@claroinvesments.com <Cl@r011235>
2023-05-24 16:16:39 +0530

    Added env pickup

--- src/main/resources/shell-scripts/stock-data.sh
+++ src/main/resources/shell-scripts/stock-data.sh
@@ -2,19 +2,19 @@
 
 LOG_DATE=$(date +%Y%m%d)
 SCRIPT_DIR=$(dirname $0)
+CONF_FILE="${HOME}/conf/env.sh"
+. ${CONF_FILE}
+
 LOG_FILE="${SCRIPT_DIR}/logs/stock-data-data-${LOG_DATE}.log"
 
 rm -f ${LOG_FILE}
 rm -f nohup.out
 
-DB_USERNAME=ENV_DB_HOST
-DB_PASSWORD=ENV_DB_PASSWORD
-DB_NAME=END_DB_NAME
 HISTORICAL_STOCK_PRICES_OUTPUT_FILE=${SCRIPT_DIR}/historical-stock-prices-output.txt
 
-export SPRING_DATASOURCE_URL="jdbc:mysql://localhost:3306/${DB_NAME}?rewriteBatchedStatements=true"
-export SPRING_DATASOURCE_USERNAME=${DB_USERNAME}
-export SPRING_DATASOURCE_PASSWORD=${DB_PASSWORD}
+export SPRING_DATASOURCE_URL="jdbc:mysql://localhost:3306/${ENV_DB_NAME}?rewriteBatchedStatements=true"
+export SPRING_DATASOURCE_USERNAME=${ENV_DB_USERNAME}
+export SPRING_DATASOURCE_PASSWORD=${ENV_DB_PASSWORD}
 export DOWNLOAD_DIR_LOC="${SCRIPT_DIR}/downloads"
 export HISTORICAL_STOCK_PRICES_OUTPUT_LOC=${HISTORICAL_STOCK_PRICES_OUTPUT_FILE}
 export NO_DAYS_BACK=7
@@ -24,6 +24,6 @@ nohup java -jar -DXmx512M -Dlogging.file.name=${LOG_FILE} -Dlogging.level.anshum
 find ${SCRIPT_DIR}/logs -name '*.log' -mtime +10 -type f -exec rm -f {} +
 
 if [ -f "${HISTORICAL_STOCK_PRICES_OUTPUT_FILE}" ]; then
-    mysql --local-infile=1  -u${DB_USERNAME} -p${DB_PASSWORD} ${DB_NAME} -e "LOAD DATA LOCAL INFILE '${HISTORICAL_STOCK_PRICES_OUTPUT_FILE}' INTO TABLE historical_stock_prices FIELDS TERMINATED BY '|' LINES TERMINATED BY '\n' IGNORE 0 LINES"
+    mysql --local-infile=1  -u${ENV_DB_USERNAME} -p${ENV_DB_PASSWORD} ${ENV_DB_NAME} -e "LOAD DATA LOCAL INFILE '${HISTORICAL_STOCK_PRICES_OUTPUT_FILE}' INTO TABLE historical_stock_prices FIELDS TERMINATED BY '|' LINES TERMINATED BY '\n' IGNORE 0 LINES"
     rm -f ${HISTORICAL_STOCK_PRICES_OUTPUT_FILE}
 fi
